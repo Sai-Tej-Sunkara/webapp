61a44009db2968dd69ad4a1bfa9163f487febe1e
sstej88 <sunkara.sai@northeastern.edu>
2023-10-01 15:44:59 -0400

    Configured CICD, and Mocha Testing for Application

---
+++ test/test_app.js
@@ -0,0 +1,35 @@
+const chai = require("chai");
+const {expect, assert} = require("chai");
+
+const chaiHttp = require("chai-http");
+
+const app = require("../app");
+const sequelize = require("../sequelize");
+
+chai.use(chaiHttp);
+
+describe("Test for Successful Server Run", ()=>{
+    it("Should Throw 405 Staus on POST request to Healthz", (done) => {
+        const server = app.listen();
+        const { address, port } = server.address();
+          chai
+            .request(`http://${address}:${3000}`)
+            .post("/healthz")
+            .end((err, res) => {
+              expect(err).to.be.null;
+              expect(res).to.have.status(405);
+              done();
+            });
+        });
+});
+
+describe("Test for Successful MySQL Connection", ()=>{
+    it('Should Connect to MySQL on Server and Authenticate', () => {
+          return sequelize.authenticate().then(()=>{
+            expect(true).to.equal(true);
+          }).catch((error)=>{
+            console.error("Unable to Connect to MySQL Server");
+            throw error;
+          })
+    });
+});
