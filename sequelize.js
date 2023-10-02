610e9ee42a8d22a34d0e897d8f3c6539a6b95912
sstej88 <sunkara.sai@northeastern.edu>
2023-10-02 01:57:48 -0400

    Passwords Bcrypted to Hash 12 and Account Created and Account Updated are ignored when sent by user

--- sequelize.js
+++ sequelize.js
@@ -15,26 +15,36 @@ const sequelize = new Sequelize(
 
 const User = sequelize.define("User", {
   id: {
-      type: sequelize.Sequelize.INTEGER,
-      autoIncrement: true,
-      primaryKey: true
+    type: sequelize.Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
   },
   first_name: {
-      type: sequelize.Sequelize.STRING,
-      allowNull: false,
+    type: sequelize.Sequelize.STRING,
+    allowNull: false,
   },
   last_name: {
-      type: sequelize.Sequelize.STRING,
-      allowNull: false,
+    type: sequelize.Sequelize.STRING,
+    allowNull: false,
   },
   email: {
-      type: sequelize.Sequelize.STRING,
-      allowNull: false,
+    type: sequelize.Sequelize.STRING,
+    allowNull: false,
   },
   password: {
-      type: sequelize.Sequelize.STRING,
-      allowNull: false,
+    type: sequelize.Sequelize.STRING,
+    allowNull: false,
   },
+  account_created: {
+    type: sequelize.Sequelize.DATE,
+    defaultValue: Sequelize.fn("NOW"),
+  },
+  account_updated: {
+    type: sequelize.Sequelize.DATE,
+    defaultValue: Sequelize.fn("NOW"),
+  },
+}, {
+  timestamps: null,
 });
 
 module.exports = { sequelize, User };
