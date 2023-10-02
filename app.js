85d1facabc8048eb26a0eb3d080f03dc92996371
sstej88 <sunkara.sai@northeastern.edu>
2023-10-02 11:21:02 -0400

    Categorized Files and Email ID is unique

--- app.js
+++ app.js
@@ -2,86 +2,9 @@
 
 const express = require("express");
 const { sequelize, User } = require("./sequelize");
-
-const fs = require("fs");
-const csv = require("csv-parser");
-const bcrypt = require("bcrypt");
+require("./file");
 
 const app = express();
-
-const filepath = "/Users/saitejsunkara/Desktop/CloudComputing/webapp/data/users.csv";
-
-let create_table_and_insert_data = (sequelize) => {
-  sequelize
-    .sync()
-    .then(() => {
-      console.log("Table created if it doesn't exist.");
-      fs.createReadStream(filepath)
-      .pipe(csv())
-      .on("data", async (data) => {
-        try {
-          // Create a new user object with only the first_name, last_name, email, and password fields
-          const newUser = {
-            first_name: data.first_name,
-            last_name: data.last_name,
-            email: data.email,
-            password: await bcrypt.hash(data.password, 12),
-          };
-
-          // Delete any other fields in the user object
-          for (const key in newUser) {
-            if (key !== "first_name" && key !== "last_name" && key !== "email" && key !== "password") {
-              delete newUser[key];
-            }
-          }
-
-          // Save the user to the database
-          const [user, created] = await User.findOrCreate({
-            where: { email: data.email },
-            defaults: newUser,
-          });
-
-          // Check if the user was created
-          if (created) {
-            console.log(`Inserted new user: ${data.first_name} ${data.last_name}`);
-          } else {
-            console.log(`User with email ${data.email} already exists, skipping.`);
-          }
-        } catch (error) {
-          console.error("Error inserting user:", error);
-        }
-      })
-      .on("end", () => {
-        console.log("CSV data processing completed");
-      })
-      .on("error", (error) => {
-        console.error("Error reading CSV: ", error);
-      });
-    })
-    .catch((error) => {
-      console.error("Error creating table: ", error);
-    });
-}
-
-sequelize.authenticate().then(()=>{
-  console.log("Database Connection Established Successfully!");
-  create_table_and_insert_data(sequelize);
-}).catch((error)=>{
-  console.error("Database Connection Haven't Been Established");
-  throw error;
-})
-
-// let csv_results = [];
-// fs.createReadStream(filepath).pipe(csv()).on("data", (data)=>{
-//   csv_results.push(data);
-// }).on("end", ()=>{
-//   console.log("CSV data: ", csv_results);
-//   if(csv_results.length == 0) {
-//       console.log("CSV File has no data");
-//   }
-// }).on("error", (error)=>{
-//   console.error("Error reading CSV: ", error);
-// })
 
 app.get("/healthz", async (req, res) => {
   if(req.headers["content-length"]>0) {
