4debe44cefce566ca1b7e7a574e908bcc6b8d6f8
sstej88 <sunkara.sai@northeastern.edu>
2023-10-02 01:06:34 -0400

    Created Table and Added CSV info in it

--- sequelize.js
+++ sequelize.js
@@ -13,4 +13,23 @@ const sequelize = new Sequelize(
   }
 );
 
-module.exports = sequelize;
+const User = sequelize.define("User", {
+  first_name: {
+    type: sequelize.Sequelize.STRING,
+    allowNull: false,
+  },
+  last_name: {
+    type: sequelize.Sequelize.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: sequelize.Sequelize.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: sequelize.Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = { sequelize, User };
