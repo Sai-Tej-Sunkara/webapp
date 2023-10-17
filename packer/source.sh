58ee9a75c0fa42ba7581ef20fd559fcab44a1b25
Sai Tej Sunkara <113054762+sstej88@users.noreply.github.com>
2023-10-17 16:53:52 -0400

    Revert "Update Source Code"
    This reverts commit f6d3409e6c29d5aa4a8ed27be01573e1223368c5.

--- packer/source.sh
+++ packer/source.sh
@@ -1,31 +1,28 @@
 #!/bin/bash
 set -e
 export DATABASE=healthz
-export HOST=127.0.0.1
-export USER=root
-export PASS=root
-export DIALECT=mysql
+export DATABASE_HOST=127.0.0.1
+export DATABASE_USERNAME=root
+export DATABASE_PASSWORD=root
 export DEBIAN_FRONTEND=noninteractive
 export DEBIAN_USER=admin
 export SERVICE_TYPE=simple
-sudo apt-get update -y
+sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install libterm-readline-gnu-perl mariadb-server ca-certificates zip unzip apt-transport-https lsb-release curl dirmngr -y
 sudo unzip webapp.zip
 sudo mysql -u root <<MYSQL_SCRIPT
-GRANT ALL PRIVILEGES ON *.* TO '$USER'@'localhost' IDENTIFIED BY '$PASS' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO '$DATABASE_USERNAME'@'localhost' IDENTIFIED BY '$DATABASE_PASSWORD' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 MYSQL_SCRIPT
-mysql -uroot -p$PASS <<SQL
-ALTER USER $USER@'localhost' IDENTIFIED BY '$PASS';
+mysql -uroot -p$DATABASE_PASSWORD <<SQL
+ALTER USER $DATABASE_USERNAME@'localhost' IDENTIFIED BY '$DATABASE_PASSWORD';
 CREATE DATABASE $DATABASE;
 FLUSH PRIVILEGES;
 SQL
 curl -sSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
-sudo apt update -y
+sudo apt update
 sudo apt install nodejs npm -y
-cd ~/webapp/
-sudo npm install
 cat <<EOF | sudo tee /etc/systemd/system/webapp.service
 [Unit]
 Description=app.js-service file to start the server instance in ec2
@@ -34,14 +31,14 @@ Wants=network-online.target
 After=network-online.target
 
 [Service]
-Environment="DATABASE=$DATABASE"
-Environment="HOST=$HOST"
-Environment="USER=$USER"
-Environment="PASS=$PASS"
+Environment="DATABASE_NAME=$DATABASE"
+Environment="DB_HOST=$DATABASE_HOST"
+Environment="MYSQL_USERNAME=$DATABASE_USERNAME"
+Environment="MYSQL_PASSWORD=$DATABASE_PASSWORD"
 Type=$SERVICE_TYPE
 User=$DEBIAN_USER
-WorkingDirectory=/home/$DEBIAN_USER/webapp/
-ExecStart=/usr/bin/node /home/$DEBIAN_USER/webapp/app.js
+WorkingDirectory=~/webapp/
+ExecStart=node app.js
 Restart=on-failure
 
 [Install]
@@ -50,5 +47,5 @@ EOF
 
 sudo chmod 755 ~/webapp/
 sudo systemctl daemon-reload
-sudo systemctl enable webapp.service
-sudo systemctl start webapp.service
+sudo systemctl enable webapp
+sudo systemctl start webapp
