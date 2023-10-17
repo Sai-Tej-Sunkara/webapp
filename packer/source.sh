096a03abdfd62120a3cfa6e0e03958b14ef10b4e
Sai Tej Sunkara <113054762+sstej88@users.noreply.github.com>
2023-10-17 16:53:52 -0400

    Revert "Updated ec2 checks"
    This reverts commit 72f7e89b00900f4a917ed3816eeeee9218846690.

--- packer/source.sh
+++ packer/source.sh
@@ -5,12 +5,12 @@ export DATABASE_HOST=127.0.0.1
 export DATABASE_USERNAME=root
 export DATABASE_PASSWORD=root
 export DEBIAN_FRONTEND=noninteractive
-export DEBIAN_USER=admin
-export SERVICE_TYPE=simple
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install libterm-readline-gnu-perl mariadb-server ca-certificates zip unzip apt-transport-https lsb-release curl dirmngr -y
+sudo apt-get install libterm-readline-gnu-perl -y
+sudo apt-get install ca-certificates zip unzip apt-transport-https lsb-release curl dirmngr -y
 sudo unzip webapp.zip
+sudo apt-get install mariadb-server -y 
 sudo mysql -u root <<MYSQL_SCRIPT
 GRANT ALL PRIVILEGES ON *.* TO '$DATABASE_USERNAME'@'localhost' IDENTIFIED BY '$DATABASE_PASSWORD' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
@@ -25,8 +25,8 @@ sudo apt update
 sudo apt install nodejs npm -y
 cat <<EOF | sudo tee /etc/systemd/system/webapp.service
 [Unit]
-Description=app.js-service file to start the server instance in ec2
-Documentation=https://fall2023.csye6225.cloud/
+Description=server.js - making your environment variables
+Documentation=https://example.com
 Wants=network-online.target
 After=network-online.target
 
@@ -35,17 +35,17 @@ Environment="DATABASE_NAME=$DATABASE"
 Environment="DB_HOST=$DATABASE_HOST"
 Environment="MYSQL_USERNAME=$DATABASE_USERNAME"
 Environment="MYSQL_PASSWORD=$DATABASE_PASSWORD"
-Type=$SERVICE_TYPE
-User=$DEBIAN_USER
-WorkingDirectory=~/webapp/
-ExecStart=node app.js
+Type=simple
+User=admin
+WorkingDirectory=/home/admin/webapp
+ExecStart=/usr/bin/node server.js
 Restart=on-failure
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
-sudo chmod 755 ~/webapp/
+sudo chmod 755 ~/webapp
 sudo systemctl daemon-reload
 sudo systemctl enable webapp
 sudo systemctl start webapp
