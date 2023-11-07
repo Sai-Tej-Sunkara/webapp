14789fb3d8436384550f7ee15d1b3dbf5364f979
sstej88 <sunkara.sai@northeastern.edu>
2023-11-06 21:01:47 -0500

    Added user changes

--- packer/source.sh
+++ packer/source.sh
@@ -12,16 +12,35 @@ sudo apt install nodejs npm -y
 cd ~/webapp/
 sudo npm install
 cd ..
-
-sudo groupadd webapp-user
-sudo useradd -m -s /bin/bash -g webapp-user webapp-user
-sudo passwd -d webapp-user
-
-sudo chown webapp-user:webapp-user -R ~/webapp/
+sudo groupadd saitejsunkara
+sudo useradd -m -s /bin/bash -g saitejsunkara saitejsunkara
+sudo passwd -d saitejsunkara
+sudo chown saitejsunkara:saitejsunkara -R ~/webapp/
 sudo chmod 755 ~/webapp/
-sudo mv ~/webapp /home/webapp-user/
+sudo mv ~/webapp /home/saitejsunkara/
 
-sudo mkdir -p /var/log/csye6225/ && sudo touch /var/log/csye6225/webapp.log
+sudo cat <<EOF | sudo tee /etc/systemd/system/webapp.service
+[Unit]
+Description=app.js
+Documentation=https://fall2023.csye6225.cloud/
+Wants=network-online.target
+After=network-online.target cloud-final.service
+
+[Service]
+EnvironmentFile=/home/saitejsunkara/.env
+Type=simple
+User=saitejsunkara
+WorkingDirectory=/home/saitejsunkara/webapp/
+ExecStart=/usr/bin/node /home/saitejsunkara/webapp/app.js
+Restart=on-failure
+
+[Install]
+WantedBy=cloud-init.target
+EOF
+sudo systemctl daemon-reload
+sudo systemctl enable webapp.service
+sudo mkdir -p /var/log/webappLogs/ && sudo touch /var/log/webappLogs/webapp.log
+
 sudo mkdir -p /opt/aws/amazon-cloudwatch-agent/etc/
  
 sudo cat <<EOF | sudo tee /opt/aws/amazon-cloudwatch-agent/etc/cloudwatch-config.json
@@ -35,7 +54,7 @@ sudo cat <<EOF | sudo tee /opt/aws/amazon-cloudwatch-agent/etc/cloudwatch-config
           "files": {
               "collect_list": [
                   {
-                      "file_path": "/var/log/csye6225/webapp.log",
+                      "file_path": "/var/log/webappLogs/webapp.log",
                       "log_group_name": "csye6225",
                       "log_stream_name": "webapp"
                   }
@@ -55,7 +74,6 @@ sudo cat <<EOF | sudo tee /opt/aws/amazon-cloudwatch-agent/etc/cloudwatch-config
 }
 }
 EOF
-
-sudo chown webapp-user:webapp-user /var/log/csye6225/webapp.log 
+sudo chown saitejsunkara:saitejsunkara /var/log/webappLogs/webapp.log
 sudo wget https://amazoncloudwatch-agent.s3.amazonaws.com/debian/amd64/latest/amazon-cloudwatch-agent.deb
 sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
